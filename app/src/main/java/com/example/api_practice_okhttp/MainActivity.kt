92b7074306ed7fd9e2ed647ae4a9d4a3fe6cba43
smin9408 <stmdaks888@naver.com>
2022-03-04 09:51:57 +0900

    xml / kt 연결 변수 = binding 변수 생성

--- app/src/main/java/com/example/api_practice_okhttp/MainActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.api_practice_okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.api_practice_okhttp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
