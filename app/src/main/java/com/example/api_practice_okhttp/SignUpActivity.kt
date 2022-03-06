cb0e5a44d895089e27116d293d7e3bf8889829be
smin9408 <stmdaks888@naver.com>
2022-03-07 03:07:05 +0900

    가입 성공시, 그 사람의 닉네임까지 파싱해서 토스트 문구로 사용

--- app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
@@ -40,8 +40,19 @@ class SignUpActivity : BaseActivity() {
 
                         if(code == 200){
 
+//                            가입한 사람의 닉네임 추출 > ~~님, 가입을 축하합니다! 토스트
+//                            회원가입화면 종료 > 로그인 화면 복귀
+                            val dataObj = jsonObj.getJSONObject("data")
+                            val userObj = dataObj.getJSONObject("user")
 
-                        }
+                            val nickname = userObj.getString("nick_name")
+
+                            runOnUiThread{
+                                Toast.makeText(mContext,"${nickname}님, 가입을 축하합니다다",Toast.LENGTH_SHORT).show()
+                            }
+//                            화면 종료 : 객체 소멸 (UI 동작이 아님)
+                            finish()
+                       }
                         else{
                             val message = jsonObj.getString("message")
 
