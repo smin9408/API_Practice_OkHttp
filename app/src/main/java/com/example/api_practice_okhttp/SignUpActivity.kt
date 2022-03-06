4441b9d6b56455eacd778223f97cc8d0d8641c50
smin9408 <stmdaks888@naver.com>
2022-03-07 00:46:16 +0900

    코틀린 단에서도 바인딩 변수 설정

--- app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
@@ -2,10 +2,15 @@ package com.example.api_practice_okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.api_practice_okhttp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
     }
 }
