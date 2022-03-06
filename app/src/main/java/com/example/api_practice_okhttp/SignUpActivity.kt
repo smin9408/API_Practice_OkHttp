10493282771fa6b8b6a8e31afba7b12887a0277c
smin9408 <stmdaks888@naver.com>
2022-03-07 00:57:10 +0900

    BaseActivity를 물려받아, 의무 구현 함수2개 작성 / onCreate에서 실제 실행

--- app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
@@ -5,16 +5,23 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.api_practice_okhttp.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     lateinit var binding: ActivitySignUpBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
     }
 
+    override fun setupEvents() {
+    }
 
+    override fun setValues() {
+    }
 
 
 
