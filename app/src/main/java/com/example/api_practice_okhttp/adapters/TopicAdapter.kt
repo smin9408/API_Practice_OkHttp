f92091c47fba2253f2603090f6b787c09656d25f
smin9408 <stmdaks888@naver.com>
2022-03-07 16:45:27 +0900

    리스트뷰 어댑터 - getView 함수 작성

--- app/src/main/java/com/example/api_practice_okhttp/adapters/TopicAdapter.kt
+++ app/src/main/java/com/example/api_practice_okhttp/adapters/TopicAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.api_practice_okhttp.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.api_practice_okhttp.R
 import com.example.api_practice_okhttp.datas.TopicData
 
 class TopicAdapter(
@@ -9,4 +13,16 @@ class TopicAdapter(
     resId: Int,
     val mList: List<TopicData>
 ) : ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
