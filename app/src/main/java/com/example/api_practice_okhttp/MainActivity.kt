965ab3a3fb93c202d78e0385285aeb797139d5aa
smin9408 <stmdaks888@naver.com>
2022-03-07 17:04:26 +0900

    JSONArray의 JSONObject들을 하나씩 변수에 담아서 > 로그로 출력

--- app/src/main/java/com/example/api_practice_okhttp/MainActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.api_practice_okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.api_practice_okhttp.databinding.ActivityMainBinding
+import com.example.api_practice_okhttp.datas.TopicData
 import com.example.api_practice_okhttp.utils.ServerUtil
 import org.json.JSONObject
 
@@ -11,6 +13,8 @@ class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    // 실제로 서버가 내려주는 주제 목록을 담을 그릇
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +29,33 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.getRequestMyInfo(mContext, object :ServerUtil.JsonResponseHandler{
+//        메인 화면 정보 가져오기 => API 호출 / 응답 처리
+        getTopicListFromServer()
+
+    }
+
+    fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
 
+//                서버가 주는 토론 주제 목록 파싱 => mTopicList에 추가하기.
                 val dataObj = jsonObj.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
 
-                runOnUiThread {
+                val topicsArr = dataObj.getJSONArray("topics")
+//                topicsArr 내부를 하나씩 추출 (JSONObject {  }) => TopicData()로 변환.
 
-                    binding.txtLoginUserNickname.text = nickname
+//                JSONArray 는 for-each 문법 지원 X. (차후 : ArrayList의 for-each 활용 예정)
+//                JAVA : for (int i = 0; i < 배열.length; i++) 와 완전히 동일한 문법.
+                for (i in 0 until topicsArr.length()) {
+
+//                    [  ] => {}, {}, {}, ... 순서에 맞는 {}를 변수에 담자.
+//                    JSON파싱의 {} => (JSONArray에게서) JSONObject로 추출.
+
+                    val topicObj = topicsArr.getJSONObject(i)
+
+                    Log.d("받아낸 주제",topicObj.toString())
+
                 }
 
             }
