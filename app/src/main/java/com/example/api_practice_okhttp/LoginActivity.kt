3e40e5d882da84993246d2a1b330ce107b1a51e7
smin9408 <stmdaks888@naver.com>
2022-03-07 09:41:48 +0900

    메인액티비티 이름 변경 > 로그인액티비티

--- app/src/main/java/com/example/api_practice_okhttp/LoginActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/LoginActivity.kt
@@ -1,21 +1,20 @@
 package com.example.api_practice_okhttp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.example.api_practice_okhttp.databinding.ActivityMainBinding
+import com.example.api_practice_okhttp.databinding.ActivityLoginBinding
 import com.example.api_practice_okhttp.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
