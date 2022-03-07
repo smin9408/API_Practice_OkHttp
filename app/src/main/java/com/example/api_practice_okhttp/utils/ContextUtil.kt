1142c04488b35d640e9b5c3707292f1f7122cd15
smin9408 <stmdaks888@naver.com>
2022-03-07 12:20:45 +0900

    getter 함수 작성

--- app/src/main/java/com/example/api_practice_okhttp/utils/ContextUtil.kt
+++ app/src/main/java/com/example/api_practice_okhttp/utils/ContextUtil.kt
@@ -18,12 +18,18 @@ class ContextUtil {
 //        TOKEN 항목에 저장 => token 항목 조회? 데이터 인식 X. 대소문자까지 동일해야함.
 //        오타를 줄이고, 코딩 편하게 하는 조치.
 
-        fun setToken( context : Context, token : String ){
+        fun setToken(context: Context, token: String) {
 
 //            메모장 파일을 열자.
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 //            입력 들어온 token 내용(TOKEN 항목에) 저장.
             pref.edit().putString(TOKEN, token).apply()
+
+        }
+
+        fun getToken(context: Context): String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(TOKEN, "")!!
 
         }
     }
