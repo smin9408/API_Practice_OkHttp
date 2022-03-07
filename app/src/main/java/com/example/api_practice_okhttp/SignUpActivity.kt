0a74af0804cf6b855c03ecc656dc6988f4e7ebb1
smin9408 <stmdaks888@naver.com>
2022-03-07 10:45:26 +0900

    이메일 중복 체크 결과 UI 반영

--- app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/SignUpActivity.kt
@@ -30,7 +30,27 @@ class SignUpActivity : BaseActivity() {
 //            서버의 죽복확인 기능 (/User_check - GET) API 활용 => ServerUtil에 함수 추가, 가져다 활용
 //            그 응답 code값에 따라 다른 문구 배치.
 
-            ServerUtil.GetRequestDuplicatedCheck("EMAIL", inputEamil, null)
+            ServerUtil.GetRequestDuplicatedCheck("EMAIL", inputEamil, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    code 값에 따라 이메일 사용 가능 여부.
+                    val code = jsonObj.getInt("code")
+                    runOnUiThread {
+                        when(code){
+                            200 -> {
+                                binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
+                            }
+                            else -> {
+                                binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요"
+                            }
+                        }
+                    }
+
+
+                    val message = jsonObj.getString("message")
+                }
+
+            })
 
         }
 
