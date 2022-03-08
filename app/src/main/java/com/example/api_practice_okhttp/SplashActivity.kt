1d7943e9f1dfbfcdb08db83fec324b42f14c4762
smin9408 <stmdaks888@naver.com>
2022-03-08 09:53:14 +0900

    로딩화면 추가 / 2.5초 뒤에 진행할 검사 주석

---
+++ app/src/main/java/com/example/api_practice_okhttp/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.example.api_practice_okhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+
+//        2.5 초가 지나면 -> 자동로그인을 해도 되는지? -> 상황에 맞는 화면으로 이동.
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+
+        myHandler.postDelayed( {
+
+//            자동로그인을 해도 되는가?
+//            1) 사용자가 자동로그인 의사를 OK 했는지?
+//            2) 로그인 시에 받아낸 토큰값이 지금도 유효한지?
+            if(){
+//                둘 다 OK라면, 바로 메인화면으로
+            }
+            else{
+//                아니라면, 로그인 화면으로.
+            }
+
+        }, 2500)
+
+    }
+}
