85e6dc939ba91c4e9dd21981d997f7a6b53cd354
smin9408 <stmdaks888@naver.com>
2022-03-08 10:48:51 +0900

    토론주제 데이터를 들고, 상세 화면 진입

--- app/src/main/java/com/example/api_practice_okhttp/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/ViewTopicDetailActivity.kt
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.api_practice_okhttp.databinding.ActivityViewTopicDetailBinding
+import com.example.api_practice_okhttp.datas.TopicData
 
-class ViewTopicDetailActivity : AppCompatActivity() {
+class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewTopicDetailBinding
+
+//    보여주게 될 토론 주제 데이터 > 이벤트처리, 데이터 표현 등 여러 함수에서 사용
+    lateinit var mTopicData : TopicData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
     }
 }
