511f121e37e762505d599e9f7d18934c97644945
smin9408 <stmdaks888@naver.com>
2022-03-08 12:07:11 +0900

    선택 진영 표현 클래스 추가

---
+++ app/src/main/java/com/example/api_practice_okhttp/datas/SideData.kt
@@ -0,0 +1,26 @@
+package com.example.api_practice_okhttp.datas
+
+import org.json.JSONObject
+
+// 토론의 선택 진영을 표현.
+
+class SideData {
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+
+        fun getSideDataFromJson(jsonObj: JSONObject): SideData {
+            val sideData = SideData()
+
+            sideData.id = jsonObj.getInt("id")
+            sideData.title = jsonObj.getString("title")
+            sideData.voteCount = jsonObj.getInt("vote_count")
+
+            return sideData
+        }
+
+    }
+
+}
