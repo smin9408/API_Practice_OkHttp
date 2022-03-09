2b367fc61a142114be26f0b31f571b1c30f9c85f
smin9408 <stmdaks888@naver.com>
2022-03-09 23:58:01 +0900

    득표 현황 새로고침  - 미리 구조를 잡아둔 함수 활용

--- app/src/main/java/com/example/api_practice_okhttp/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/api_practice_okhttp/ViewTopicDetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.api_practice_okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.api_practice_okhttp.databinding.ActivityViewTopicDetailBinding
@@ -34,13 +35,22 @@ class ViewTopicDetailActivity : BaseActivity() {
             ServerUtil.postRequestVote(mContext, mTopicData.sideList[0].id, object : ServerUtil.JsonResponseHandler{
                 override fun onResponse(jsonObj: JSONObject) {
 
+//                    토스트로, 서버가 알려준 현재 상황 (신규 투표 or 재투표 or 취소 등)
+                    val message = jsonObj.getString("message")
+
+                    runOnUiThread {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                    }
+
+//                    변경된 득표 현황을 다시 불러오자.
+                    getTopicDetailFromServer()
                 }
 
             })
 
 
 
-//            투표 현황 새로고침 (응답)
+//           투표 현황 새로고침 (응답)
 
         }
 
